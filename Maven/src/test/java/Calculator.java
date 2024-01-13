7901c649e0fa07265827e97ddb75387c90a9d026
benediktfaude@gmail.com <1b2e3n7@Ben>
2024-01-13 15:24:30 +0100

    Fertige Aufgabe 03

---
+++ Maven/src/test/java/Calculator.java
@@ -0,0 +1,23 @@
+/**
+ * Schnittstelle für eine einfache Taschenrechner-Klasse.
+ *
+ * Implementiert die Additionsfunktion von Integer-Zahlen.
+ */
+public interface Calculator {
+
+    /**
+     * Addiert zwei Integer-Zahlen.
+     *
+     * @param a Die erste Zahl.
+     * @param b Die zweite Zahl.
+     * @return Die Summe der beiden Zahlen.
+     */
+    int add(int a, int b);
+
+    int subtract(int i, int i1);
+
+    int multiply(int i, int i1);
+
+    int divide(int i, int i1);
+
+}
---
+++ Maven/src/test/java/CalculatorTest.java
@@ -0,0 +1,55 @@
+import org.junit.Test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        SimpleCalculator calculator = new SimpleCalculator();
+        int result = calculator.add(1, 2);
+        assertEquals(3, result);
+    }
+
+    @Test
+    public void testSubtract() {
+        SimpleCalculator calculator = new SimpleCalculator();
+        int result = calculator.subtract(1, 2);
+        assertEquals(-1, result);
+    }
+
+    @Test
+    public void testMultiply() {
+        SimpleCalculator calculator = new SimpleCalculator();
+        int result = calculator.multiply(1, 2);
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void testDivide() {
+        SimpleCalculator calculator = new SimpleCalculator();
+        int result = calculator.divide(2, 1);
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void testDivideByZero() {
+
+        SimpleCalculator calculator = new SimpleCalculator();
+        int result = calculator.divide(2, 0);
+        assertEquals(0, result);
+    }
+
+
+    @Test
+    public void testOverflow() {
+        SimpleCalculator calculator = new SimpleCalculator();
+        int result = calculator.add(Integer.MAX_VALUE, 1);
+        assertEquals(Integer.MIN_VALUE, result);
+    }
+
+}
