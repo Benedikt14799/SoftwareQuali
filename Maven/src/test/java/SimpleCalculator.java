fb64efb9627c92be412d79623d840e64143b3bf4
benediktfaude@gmail.com <1b2e3n7@Ben>
2024-01-13 15:24:30 +0100

    Fertige Aufgabe 03

---
+++ Maven/src/test/java/SimpleCalculator.java
@@ -0,0 +1,29 @@
+public class SimpleCalculator implements Calculator {
+
+    @Override
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    @Override
+    public int divide(int i, int i1) {
+        if (i1 == 0) {
+            return 0;
+
+        } else
+            return i / i1;
+    }
+
+
+    @Override
+    public int multiply(int i, int i1) {
+        return i * i1;
+    }
+
+    @Override
+    public int subtract(int i, int i1) {
+        return i - i1;
+    }
+}
+
+
